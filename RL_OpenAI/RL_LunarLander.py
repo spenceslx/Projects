00d28400762ea084523d0033db207194bc274fe9
spenceslx <spenceslx@hotmail.com>
2018-04-09 17:08:35 -0400

    Pushing before I might change gears, looking to implement A3C with train() as a mappable function in this commit.

--- RL_OpenAI/RL_LunarLander.py
+++ RL_OpenAI/RL_LunarLander.py
@@ -9,6 +9,8 @@ from torch.autograd import Variable
 import torch.nn as nn
 import torch.nn.functional as F
 
+#Lunar Lander State Vector:
+#[pos_x, pos_y, vel_x, vel_y, angle, vel_angle, left_leg_contact, right_leg_contact]
 
 class Net(nn.Module):
     """ Building 3 linear layer net to give an action given an environment
@@ -30,7 +32,8 @@ class Net(nn.Module):
     def forward(self, x):
         x = F.relu(self.l1(x))
         x = F.relu(self.l2(x))
-        x = F.sigmoid(self.l3(x)) #[0,1] activation
+        x = F.relu(self.l3(x))
+        return x
 
 def predict_action(var):
     """ Converts Net output Variable from a "continous onehot encoding" into
@@ -38,7 +41,7 @@ def predict_action(var):
         -- input:
             - var: The policy function output, which is a Net output,
                    a torch.autograd.Variable
-        -- output:
+        -- output, for LunarLander:
             - 0: Do nothing
             - 1: fire_left
             - 2: fire_right
@@ -50,39 +53,56 @@ def predict_action(var):
     return action
 
 
-def train(policy, num_episodes=num_episodes, max_frames=max_frames):
+def train(policy, expected_reward):
     """ Trains a policy to take an action giving an environment observation,
         with the goal of maximizing the rewards from each trial
         -- input:
             - policy: A neural net of class Net
-            - num_episodes: Number of episodes to train for (env.reset()'s)
-            - max_frames: Maximum number of frames for an episode to run for
+            - expected_reward: A neural net of class Net
         -- output:
             - policy: A trained neural net of class Net
+            - expected_reward: A trained neural net of class Net
     """
     env = gym.make('LunarLander-v2')
     criterion = nn.MSELoss()
     for episode in range(0, num_episode):
-        observation = env.reset()
-        for frame (0, max_frames):
-
+        state = env.reset()
+        frame, cum_reward, grad_policy, grad_reward = 0, 0, 0, 0
+        isterminal = False
+        state_store = [0 for x in range(0, max_frames)]
+        action_store = [0 for x in range(0, max_frames)]
+        reward_store = [0 for x in range(0, max_frames)]
+        while (isterminal != True) or (frame < max_frames):
+            action = policy(state)
+            state, reward, isterminal, more_info = env.step(action)
+            state_store[frame] = state
+            action_store[frame] = action
+            reward_store[frame] = reward
+            frame += 1
+        if isterminal == True:
+            cum_reward = expected_reward[reward] #Bootstrap from last state
+        #now accumulate gradients, rewards
+        while frame > 0:
+            cum_reward = reward_store[timestamp] + (discount_factor*cum_reward)
+            grad_policy = grad_policy +
+            frame -= 1
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description=\
                         'Reinforcement Learning on LunarLander')
-    parser.add_argument('mode', type=string, help='train, test, or demo')
+    parser.add_argument('mode', type=string, help='Train, test, or demo')
     parser.add_argument('--num_episodes', type=int, default=2000,\
                         help='Number of episodes (env.resets())to train on')
     parser.add_argument('--max_frames', type=int, default=1000,\
                         help='Maximum number of frames in the environment')
+    parser.add_argument('--discount_factor', type=float32, default=0.99,\
+                        help='Discount of previous rewards (gamma)')
     args = parser.parse_args()
 
     if mode == 'train':
         #environment observation is np.array length 8
         #environment step is np.array length 4
-        n_feature = 8
-        n_out = 4
-        n_hidden = 10
-        policy = Net(n_feature, n_out, n_hidden)
-        policy = train(Net)
+        policy = Net(n_feature=8, n_out=4, n_hidden=10)
+        expected_reward = Net(n_feature=8, n_out=1, n_hidden=10)
+        policy = train(policy, expected_reward)
