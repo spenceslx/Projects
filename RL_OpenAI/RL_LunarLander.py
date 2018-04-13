6139c94ce6ec4e733f631580f5ab2b54458f9145
spenceslx <spenceslx@hotmail.com>
2018-04-13 15:58:47 -0400

    Changed approach, combining both networks into a single ActorCritic class. Set up the network structures and forwarding relationships. Still have to create train methods for each network (which looks like will involve splitting up the layer params for each network and freezing the opposite network when training). Also have to implement random sampling from saved memory from each episode. After all that is done, will fix the train function.

--- RL_OpenAI/RL_LunarLander.py
+++ RL_OpenAI/RL_LunarLander.py
@@ -8,52 +8,84 @@ from time import time
 from torch.autograd import Variable
 import torch.nn as nn
 import torch.nn.functional as F
+import torch.optim as optim
 
 #Lunar Lander State Vector:
 #[pos_x, pos_y, vel_x, vel_y, angle, vel_angle, left_leg_contact, right_leg_contact]
 
-class Net(nn.Module):
-    """ Building 3 linear layer net to give an action given an environment
+class ActorCritic(nn.Module):
+    """ Creates a functionally merged Actor and Critic network models, both
+        built of fully connected layers
         -- input:
-            - n_feature: number of input features (int)
-            - n_out: number of output features (int)
-            - n_hidden (optional): number nodes in hidden layer (int)
-        -- output: a nn.autograd.Variable of size n_out
+            - env_space: Size of input vector from environment(int)
+            - action_space: Size of output vector for action softmax onehot
+            - n_hidden (optional): Number nodes in hidden layers (int)
+        -- output:
+            - env: The environment vector given to the models
+            - act: The softmax onehot encoded action vector
+            - expected_value: The expected value from the state-action pair
     """
-    def __init__(self, n_feature, n_out, n_hidden=10):
+    def __init__(self, env_space, action_space, n_hidden=6, mode=args.mode):
         super(Net, self).__init__()
-        self.l1 = nn.Linear(n_feature, n_hidden)
-        self.l2 = nn.Linear(n_hidden, n_hidden)
-        self.l3 = nn.Linear(n_hidden, n_out)
-        self.n_feature = n_feature
+        self.action_space = action_space
+        self.env_space = env_space
         self.n_hidden = n_hidden
-        self.n_out = n_out
+        self.Actor()
+        self.Critic()
 
-    def forward(self, x):
-        x = F.relu(self.l1(x))
-        x = F.relu(self.l2(x))
-        x = F.relu(self.l3(x))
-        return x
+    def Actor(self):
+        self.l1 = nn.Linear(env_space, n_hidden)
+        self.l2 = nn.Linear(n_hidden, n_hidden)
+        self.l3 = nn.Linear(n_hidden, action_space)
 
-def predict_action(var):
-    """ Converts Net output Variable from a "continous onehot encoding" into
-        an integer for action determination
-        -- input:
-            - var: The policy function output, which is a Net output,
-                   a torch.autograd.Variable
-        -- output, for LunarLander:
-            - 0: Do nothing
-            - 1: fire_left
-            - 2: fire_right
-            - 3: fire_main
-    """
-    _, indicies = var.data.max(0)
-    #return a max action if there are more than one
-    action = indicies[randint(0, len(indicies)-1)]
-    return action
+    def Critic(self):
+        self.evn1 = nn.Linear(env_spave, n_hidden)
+        self.env2 = nn.Linear(n_hidden, n_hidden)
+        self.act1 = nn.Linear(action_space, n_hidden)
+        self.merged1 = nn.Linear(2*n_hidden, n_hidden)
+        self.merged2 = nn.Linear(n_hidden, 1)
 
+    def forward(self, env):
+        act = self.ActorForward(env)
+        expected_value = self.CriticForward(env, act)
+        return env, act, expected_value
 
-def train(policy, expected_reward):
+    def ActorForward(self, env):
+        act = F.relu(self.l1(env))
+        act = F.relu(self.l2(act))
+        act = F.softmax(self.l3(act))
+        return act
+
+    def CriticForward(self, env, act):
+        env = F.relu(self.env1(env))
+        env = F.relu(self.env2(x))
+        act = F.relu(self.act1(act))
+        merged = torch.cat((env, act), 0)
+        merged = F.relu(self.merged1(merged))
+        expected_value = F.relu(self.merged2(merged))
+        return expected_value
+
+    def predict_action(self, state):
+        """ Converts Net output Variable from a "softmax onehot encoding" into
+            an integer for action determination
+            -- input:
+                - var: The policy function output, which is a Net output,
+                       a torch.autograd.Variable
+            -- output, for LunarLander:
+                - 0: Do nothing
+                - 1: fire_left
+                - 2: fire_right
+                - 3: fire_main
+        """
+        self.eval()
+        possible_actions = self.forward(state).data
+        action = torch.distribution.Categorical(possible_actions).sample().numpy()[0]
+        #_, indicies = var.data.max(0)
+        #return a max action if there are more than one
+        #action = indicies[randint(0, len(indicies)-1)]
+        return action
+
+def train(actor_critic):
     """ Trains a policy to take an action giving an environment observation,
         with the goal of maximizing the rewards from each trial
         -- input:
@@ -64,16 +96,18 @@ def train(policy, expected_reward):
             - expected_reward: A trained neural net of class Net
     """
     env = gym.make('LunarLander-v2')
+    optimizer = optim.RMSprop(policy.parameters(), args.lr, eps=args.eps, alpha=args.alpha)
     criterion = nn.MSELoss()
-    for episode in range(0, num_episode):
+    actor_critic.train()
+    for episode in range(0, args.num_episode):
         state = env.reset()
         frame, cum_reward, grad_policy, grad_reward = 0, 0, 0, 0
         isterminal = False
-        state_store = [0 for x in range(0, max_frames)]
-        action_store = [0 for x in range(0, max_frames)]
-        reward_store = [0 for x in range(0, max_frames)]
-        while (isterminal != True) or (frame < max_frames):
-            action = policy(state)
+        state_store = [0 for x in range(0, args.max_frames)]
+        action_store = [0 for x in range(0, args.max_frames)]
+        reward_store = [0 for x in range(0, args.max_frames)]
+        while (isterminal != True) or (frame < args.max_frames):
+            action = predict_action(policy(state))
             state, reward, isterminal, more_info = env.step(action)
             state_store[frame] = state
             action_store[frame] = action
@@ -82,7 +116,7 @@ def train(policy, expected_reward):
         if isterminal == True:
             cum_reward = expected_reward[reward] #Bootstrap from last state
         #now accumulate gradients, rewards
-        while frame > 0:
+        while frame >= 0:
             cum_reward = reward_store[timestamp] + (discount_factor*cum_reward)
             grad_policy = grad_policy +
             frame -= 1
@@ -98,11 +132,16 @@ if __name__ == "__main__":
                         help='Maximum number of frames in the environment')
     parser.add_argument('--discount_factor', type=float32, default=0.99,\
                         help='Discount of previous rewards (gamma)')
+    parser.add_argument('--lr', type=float32, default=0.99,\
+                        help='Learning rate for RMSprop optimizer')
+    parser.add_argument('--alpha', type=float32, default=0.99,\
+                        help='Alpha for RMSprop optimizer')
+    parser.add_argument('--eps', type=float32, default=1e-5,\
+                        help='Epsilon for RMSprop optimizer')
     args = parser.parse_args()
 
     if mode == 'train':
         #environment observation is np.array length 8
         #environment step is np.array length 4
-        policy = Net(n_feature=8, n_out=4, n_hidden=10)
-        expected_reward = Net(n_feature=8, n_out=1, n_hidden=10)
-        policy = train(policy, expected_reward)
+        actor_critic = ActorCritic(env_space=8, action_space=4)
+        actor_critic = train(actor_critic)
